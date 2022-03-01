544d5ed0498d9b076df4cb287d542db79c0f02eb
lamodots <lamodots@gmail.com>
2022-03-01 07:05:30 -0800

    Updated and fixed and the pagination link

--- js/apicall.js
+++ js/apicall.js
@@ -196,6 +196,19 @@ function getMovies(url){
             prevPage = currentPage - 1;
           totalPages = data.total_pages;
 
+          current.innerText = currentPage;
+          if(currentPage <= 1){
+            prev.classList.add("disabled");
+            next.classList.remove("disabled");
+          }else if(currentPage >= totalPages){
+            prev.classList.remove("disabled");
+            next.classList.add("disabled");
+          }else{
+            prev.classList.remove("disabled");
+            next.classList.remove("disabled");
+          }
+          tagsElem.scrollIntoView({behavior: "smooth"});
+
         }else{
             main.innerHTML = `<h1 class="noresult">NO results found</h1>`
         }
@@ -267,18 +280,31 @@ if(searchTerm){
 
 })
 
+prev.addEventListener("click", ()=>{
+  if(prevPage > 0){
+    pageCall(prevPage);
+  }
+  })
+
 next.addEventListener("click", ()=>{
 if(nextPage <= totalPages){
   pageCall(nextPage);
 }
-
 })
 
 function pageCall(page){
  let urlSplit = lastUrl.split("?");
  let queryParams = urlSplit[1].split("&");
- let key =queryParams[queryParams.length -1].split("=");
+ let key = queryParams[queryParams.length -1].split("=");
  if(key[0] != "page"){
-   
+  let url = lastUrl + "&page="+page;
+  getMovies(url);
+ }else{
+   key[1]=page.toString();
+   let a = key.join("=");
+   queryParams[queryParams.length -1] = a;
+   let b =queryParams.join("&");
+   let url = urlSplit[0]+ "?"+ b;
+   getMovies(url);
  }
 }
